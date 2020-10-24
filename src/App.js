5945a13a9a4d42af0206afe7263eb2b679b035ff
gus <gustavo.segovia03@gmail.com>
2020-10-23 23:42:07 -0300

    modificado codigo de la seccion conocimientos

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Portfolio from './Portfolio'
-
-
+import TarjetasConocimientos from './TarjetasConocimientos'
+import {faHtml5} from '@fortawesome/free-brands-svg-icons'
 function App() {
   return (
     <div className="App ">
       <Portfolio />
+      <TarjetasConocimientos icono={faHtml5}/>
+      {console.log(faHtml5)}
     </div>
   );
 }
--- src/TarjetasConocimientos.jsx
+++ src/TarjetasConocimientos.jsx
@@ -2,15 +2,15 @@ import React from 'react';
 import './Tarjetaconocimiento.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
-const TarjetasConocimientos = props =>(
-    <div class="card text-center m-4 p-2 shadow">
+const TarjetasConocimientos = props=>(
+    <div className="card text-center m-4 p-2 shadow">
       <div className="encabezado d-flex justify-content-center">
-    <FontAwesomeIcon icon={props.icono} size="6x" color={props.color}/>
+     <FontAwesomeIcon icon={props.icono} size="6x" color={props.color}/>
     </div>
-    <div class="card-body ">
-      <h5 class="card-title h2"><span>{props.title}</span></h5>
-      <p class="card-text">{props.description}</p>
-      <a href={props.enlace} class="btn btn-outline-dark" target='_blank'>Go somewhere</a>
+    <div className="card-body ">
+      <h5 className="card-title h2">{props.title}</h5>
+      <p className="card-text">{props.description}</p>
+      <a href='...' className="btn btn-outline-dark">Go somewhere</a>
     </div>
   </div>
 )
