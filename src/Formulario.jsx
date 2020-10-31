986eddcb8aedb900c364e6885dca69c1d1d2a778
gus <gustavo.segovia03@gmail.com>
2020-10-31 11:42:54 -0300

    Manejo estado del formulario

--- src/Formulario.jsx
+++ src/Formulario.jsx
@@ -9,37 +9,47 @@ class Formulario extends Component{
         }
         this.cambiarNombre = this.cambiarNombre.bind(this)
         this.cambiarCorreo = this.cambiarCorreo.bind(this)
+        this.volverNombre = this.volverNombre.bind(this)
+        this.volverCorreo = this.volverCorreo.bind(this)
     }
-    cambiarNombre(e){
+    cambiarNombre(){
         this.setState({
-            nombre: e.target.value
+            nombre: 'Inserte su nombre'
         })
     }
-    cambiarCorreo(e){
+    cambiarCorreo(){
         this.setState({
-            correo: e.target.value
+            correo: 'Inserte correo de contacto'
+        })
+    }
+    volverNombre(){
+        this.setState({
+            nombre: ''
+        })
+    }
+    volverCorreo(){
+        this.setState({
+                correo: ''
         })
     }
     render(){
         return (
             <div className="formulario">
             <form className='p-2 w-50 m-auto'>
-                <div className="form-group" >
+                <div className="form-group my-3" >
                     <label for="exampleInputEmail1">Nombre</label>
                     <input type="text" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"
-                    onChange={this.cambiarNombre}/>
+                    onFocus={this.cambiarNombre} onBlur={this.volverNombre}/>
+                    <small className='text-danger'>{`${this.state.nombre}`}</small>
                 </div>
-                <div className="form-group">
+                <div className="form-group my-3">
                     <label for="exampleInputPassword1">Dirrecion de eMail</label>
                     <input type="email" className="form-control" id="exampleInputPassword1"
-                    onChange={this.cambiarCorreo}/>
+                    onFocus={this.cambiarCorreo} onBlur={this.volverCorreo}/>
+                    <small className='text-danger'>{`${this.state.correo}`}</small>
                 </div>
                 <button type="submit" className="btn btn-outline-dark">Enviar</button>
             </form>
-            <div className="componenteQueCambia m-auto w-50" >
-                <h2>{`Hola ${this.state.nombre}` }</h2>
-                <span>{`Tu correo es ${this.state.correo}`}</span>
-            </div>
             </div>
         )
     }
